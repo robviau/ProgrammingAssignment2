0db515bf19ac1796ea770e220ffd55129fdca992
rav42@case.edu <rav42@case.edu>
2015-07-22 22:30:17 -0500

    hw

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL #initializing variable to store matrix
+  set <- function(y){ 
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x # just returns the matrix
+  revmatrix <-function(rev) m<<- solve #store reversed matrix to m
+  getmatrix <- function() m
+  list(set = set, get = get,
+       revmatrix = revmatrix,
+       getmatrix = getmatrix) # make list vector of functions
 }
 
 
@@ -12,4 +22,42 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix() #read the memory for the matrix
+  if(!is.null(m)) { #if reversed data present, just return it
+    #print("getting cached data")
+    return(m)
+  }
+  data <- x$get() # calculate if reverse matrix not present
+  m <- solve(data, ...)
+  x$revmatrix(m)
+  m
+
+    
+}
+
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    #print("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
